f687dcff8679637ebaa344db7dd557cccc1bae71
Denis <strigodenis@gmail.com>
2024-01-26 19:45:45 +0300

    add verbose name to app

--- compbatya/comp_service/apps.py
+++ compbatya/comp_service/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CompServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comp_service'
+    verbose_name = 'Компьютерный сервис'
