339d84f37bc2b2ec43f5a11cb15f148417966087
Denis <strigodenis@gmail.com>
2024-01-26 19:48:51 +0300

    feat: add token authentication

--- compbatya/compbatya/settings.py
+++ compbatya/compbatya/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'comp_service.apps.CompServiceConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
--- compbatya/compbatya/urls.py
+++ compbatya/compbatya/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('comp_service.urls'))
+    path('api/v1/', include('comp_service.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
