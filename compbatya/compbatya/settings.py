51c9eaddf90ea1beb7f6f6ce81c8ddabea7b4c76
Denis <strigodenis@gmail.com>
2024-02-15 19:34:39 +0300

    feat: add media folder

--- compbatya/compbatya/settings.py
+++ compbatya/compbatya/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 import environ
 
 from pathlib import Path
@@ -136,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
