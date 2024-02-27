92be3d7a645a9e703dbe1047166f1f190b5d7667
Denis <strigodenis@gmail.com>
2024-02-27 19:32:32 +0300

    feat: add send mail task

---
+++ compbatya/comp_service/tasks.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.core.mail import send_mail
+from celery import shared_task
+
+
+
+@shared_task
+def send_mail_to_managers(phone: str, name: str,
+                          client_mail: str = None) -> None:
+    Users = get_user_model()
+    emails = Users.objects.filter(is_staff=True).values_list('email')
+    emails = [i[0] for i in emails if i[0]]
+    msg_text = f"Новая заявка!!!\n\nТелефон: {phone}\nКак обращаться: {name}\n" \
+        f"E-mail: {client_mail if client_mail else '-'}"
+        
+    send_mail(
+        subject='Заявка на ремонт',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        message=msg_text,
+        recipient_list=emails,
+        fail_silently=False,
+    )
