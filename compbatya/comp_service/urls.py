318df4a554172e41aaa067f680e968f1aea5f36e
Denis <strigodenis@gmail.com>
2024-03-04 01:49:07 +0300

    refactor: create router

--- compbatya/comp_service/urls.py
+++ compbatya/comp_service/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
+from rest_framework import routers
 from .views import *
 
+
+router = routers.SimpleRouter()
+router.register('devices', DevicesViewSet)
+router.register('requests', RequestsViewSet)
 
 
 urlpatterns = [
     path('specialists/', SpecialistsAPIList.as_view(), name='specialists-all'),
     path('services/', ServicesAPIList.as_view(), name='services-all'),
-    path('devices/', DevicesAPIList.as_view(), name='devices-all'),
     path('clients/', CreateClient.as_view(), name='create-client'),
-    path('requests/', RequestsApiList.as_view(), name='requests-all'),
     path('specialists/<slug:profile>/', SpecialistsAPIList.as_view(), name='specialists'),
     path('services/<slug:profile>/', ServicesAPIList.as_view(), name='services'),
-    path('create-request/', CreateRequest.as_view(), name='create-request'),
-    path('request/<int:pk>/', DeleteUpdateRequest.as_view(), name='request'),
-    path('device/', CreateDevice.as_view(), name='create-device'),
-    path('device/<int:pk>', UpdateDevice.as_view(), name='update-device'),
 ]
+
+urlpatterns += router.urls
